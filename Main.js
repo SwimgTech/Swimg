db66da946bd13e5d7fb07d7a5b23eeaf5d6cf2d6
Lev112233 <74585578+Lev112233@users.noreply.github.com>
2021-01-18 18:12:16 +0300

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+
+let dictionary = require(".dictionary.js").DICTIONARY;
+let lexer = require("./main.js").LEXER;
+let parser = require("./main.js").PARSER;
+
+global.vars = [];
+
+fs.readFile("../test/test.lang", "utf-8", function (error, content) {
+
+	if (error === null) {
+		let lexems = lexer(content, dictionary);
+		let parsedString = parser(lexems, dictionary);
+
+		console.log(parsedString);
+
+
+	}
+	else {
+		console.error("ERROR!");
+		console.error(error);
+	}
+
+});
+
+//I CODED THIS FULLY
